b2f6f45ef278367d5b55230c05c0c173bbdd8781
Кобылкевич Фёдор <fmkobylkevich@mai.education>
2026-02-28 01:13:37 +0300

    add cli and readme.md, add tests

--- lab1/main.c
+++ lab1/main.c
@@ -1,5 +1,38 @@
 #include "tree.c"
 
 signed main() {
+    node *tree = NULL;
 
+    int choice, value;
+
+    printf("1. Добавить узел\n");
+    printf("2. Удалить узел\n");
+    printf("3. Вывести дерево\n");
+    printf("4. Проверить, все ли листья на одном уровне\n");
+    printf("0. Выход\n");
+    printf("Выбор: ");
+    scanf("%d", &choice);
+
+    while (choice != 0) {
+        switch (choice) {
+            case 1:
+                printf("Введите значение:\n");
+                scanf("%d", &value);
+                tree = insert(tree, value);
+                break;
+            case 2:
+                printf("Введите значение:\n");
+                scanf("%d", &value);
+                tree = erase(tree, value);
+                break;
+            case 3:
+                print(tree, 0);
+                break;
+            case 4:
+                printf("%d\n", query(tree, 1, depth(tree)));
+                break;
+        }
+        printf("Выбор: ");
+        scanf("%d", &choice);
+    }
 }
--- lab1/tests.c
+++ lab1/tests.c
@@ -6,24 +6,16 @@ void test1() {
     printf("test_1:\n");
     node *tree = NULL;
     tree = insert(tree, 10);
-    tree = insert(tree, 20);
-    tree = insert(tree, 20);
-    tree = insert(tree, 9);
-    tree = insert(tree, 22);
-    tree = insert(tree, 23);
-    tree = insert(tree, 8);
-    tree = insert(tree, 7);
-
     assert(tree->data == 10);
+    tree = insert(tree, 20);
+    assert(tree->r->data == 20);
+    tree = insert(tree, 20);
+    assert(tree->r->data == 20);
+    tree = insert(tree, 9);
+    assert(tree->l->data == 9);
 
-    tree = erase(tree, 7);
-    tree = erase(tree, 10);
+    // assert(query(tree, 1, depth(tree)) == 1);
 
-    assert(tree->data == 20);
-
-    assert(query(tree, 1, depth(tree)) == 1);
-
-    print(tree, 0);
     clearTree(tree);
 }
 
@@ -45,14 +37,52 @@ void test2() {
 
 void test3() {
     printf("test_3:\n");
+    node *tree = NULL;
+    assert(erase(tree, 100000) == NULL);
+    tree = insert(tree, 10);
+    tree = insert(tree, 20);
+    tree = insert(tree, 20);
+    tree = insert(tree, 9);
+    tree = insert(tree, 22);
+    tree = insert(tree, 23);
+    tree = insert(tree, 8);
+    tree = insert(tree, 7);
+    assert(tree->data == 10);
+    tree = erase(tree, 7);
+    tree = erase(tree, 10);
+    assert(tree->data == 20);
+    clearTree(tree);
 }
 
 void test4() {
     printf("test_4:\n");
+    node *tree = NULL;
+    assert(query(tree, 1, depth(tree)) == 1);
+    tree = insert(tree, 10);
+    tree = insert(tree, 20);
+    tree = insert(tree, 20);
+    tree = insert(tree, 9);
+    assert(query(tree, 1, depth(tree)) == 1);
+    tree = insert(tree, 1);
+    assert(query(tree, 1, depth(tree)) == 0);
+    tree = insert(tree, 21);
+    assert(query(tree, 1, depth(tree)) == 1);
+    print(tree, 0);
 }
 
 void test5() {
     printf("test_5:\n");
+    node *tree = NULL;
+    assert(findMin(tree) == NULL);
+    tree = insert(tree, 10);
+    tree = insert(tree, 20);
+    tree = insert(tree, 20);
+    assert(findMin(tree)->data == 10);
+    tree = insert(tree, 8);
+    assert(findMin(tree)->data == 8);
+    tree = insert(tree, 9);
+    tree = insert(tree, 1);
+    assert(findMin(tree)->data == 1);
 }
 
 signed main() {
